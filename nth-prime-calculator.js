a6bb979e570ccdc28b5549a4d4cb0a351121508d
Daniel Hug <danielthehug@gmail.com>
2014-04-20 15:20:21 -0700

    Add JS file

---
+++ nth-prime-calculator.js
@@ -0,0 +1,16 @@
+function getNthPrime(n) {
+	var primes;
+	l: for (primes = [2], i = 3, root; primes.length < n; i += 2) {
+		for (root = Math.sqrt(i), j = 0; primes[j] <= root; j++) {
+			if (i%primes[j] === 0) continue l;
+		}
+		primes.push(i);
+	}
+	return primes[n - 1];
+}
+
+console.log('Wait for it...');
+
+console.time('Time it took');
+console.log('The ten millionth prime number is: ' + getNthPrime(10000000));
+console.timeEnd('Time it took');
