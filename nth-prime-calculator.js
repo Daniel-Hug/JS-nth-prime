8cfedc4ed0de6e3b5c4fb7d9b8be8ec555c12cec
Daniel Hug <daniel@hugwebdesign.com>
2017-04-12 11:33:20 -0700

    fix undefined var

--- nth-prime-calculator.js
+++ nth-prime-calculator.js
@@ -1,6 +1,5 @@
 function getNthPrime(n) {
-	var primes;
-	l: for (primes = [2], i = 3, root; primes.length < n; i += 2) {
+	l: for (var primes = [2], i = 3, root; primes.length < n; i += 2) {
 		for (root = Math.sqrt(i), j = 0; primes[j] <= root; j++) {
 			if (i%primes[j] === 0) continue l;
 		}
